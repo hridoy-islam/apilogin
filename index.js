fa89ffb7e74195d3505a4ac1a5582b4410e8e641
hridoy-islam <hridoy4t@gmail.com>
2023-06-07 07:30:31 +0600

    Login & Register

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const loginRoute = require("./Routes/loginRoute");
+const { isAuthenticated } = require("./config/helper")
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/auth', loginRoute)
+
+app.get("/protected", isAuthenticated, (req, res) => {
+  res.json({
+    success: true, msg: "Welcome user!!", email: req.email
+  });
+});
+
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected!!!!');
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server is running: ${port}`);
+});
